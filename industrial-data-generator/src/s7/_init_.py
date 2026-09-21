afc26ded05a0f113cc79a4c749506b8c9eec7e49
Cristiano <ladrilhandodata@gmail.com>
2026-09-21 19:18:50 -0300

    Add init file for s7 module with exports

---
+++ industrial-data-generator/src/s7/_init_.py
@@ -0,0 +1,9 @@
+from modbus.factory import build_modbus_reference_model
+from modbus.model import ModbusReferenceModel
+from modbus.runtime import ModbusRuntime
+
+__all__ = [
+    "build_modbus_reference_model",
+    "ModbusReferenceModel",
+    "ModbusRuntime",
+]
